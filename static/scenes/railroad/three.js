2666cbda7260e48ddff3b65ccc94ce56339366f1
alboxer2000 <20brigidialessandro@icxxsettembrerimini.edu.it>
2025-01-07 20:13:48 +0100

    Delete static directory

--- static/scenes/railroad/three.js
+++
@@ -1,8 +0,0 @@
-import * as THREE from "three";
-import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader.js";
-import {OBJLoader} from "three/examples/jsm/loaders/OBJLoader.js";
-import {DRACOLoader} from "three/examples/jsm/loaders/DRACOLoader.js";
-
-const exportedThree = {GLTFLoader, OBJLoader, DRACOLoader};
-Object.assign(exportedThree, THREE);
-globalThis.THREE = exportedThree;
